b858475a85e72b15313021c5132eae83679a947f
nekoLin <krusiawanbudi1@gmail.com>
2026-07-24 02:47:16 +0800

    chore: railway setup

--- admin/config/logging.php
+++ admin/config/logging.php
@@ -54,7 +54,8 @@ return [
 
         'stack' => [
             'driver' => 'stack',
-            'channels' => explode(',', (string) env('LOG_STACK', 'single')),
+            'channels' => ['stderr'],
+            // 'channels' => explode(',', (string) env('LOG_STACK', 'single')),
             'ignore_exceptions' => false,
         ],
 
